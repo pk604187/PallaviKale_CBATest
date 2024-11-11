87a4acac82152b0b47ee7142cf0d4f8b10a493c5
pk604187 <pallavi.kale@tpgtelecom.com.au>
2024-11-11 11:59:28 +1100

    added 1 more test script

--- tests/PetStore_Pet_Get.spec.ts
+++ tests/PetStore_Pet_Get.spec.ts
@@ -19,3 +19,12 @@ test('Get pet by ID @Get' , async({request}) =>{
     console.log(receivedPetdata.id);
     expect(receivedPetdata.id.toString()).toEqual(`${petData.petID}`);
 })
+
+test('Get all pets by status 1 @Get1' , async({request}) =>{
+    //console.log(request)
+    const allPetsbyStatus = await request.get(`https://petstore.swagger.io/v2/pet/findByStatus?status=${petData.PetStatus_Available}`);
+    expect(allPetsbyStatus.ok()).toBeTruthy();
+    const data = await allPetsbyStatus.json();
+    console.log(data);
+    //console.log("Ruby" + Math.floor(100000 + Math.random() * 900000).toString())
+})
